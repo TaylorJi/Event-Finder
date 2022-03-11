a1bd4898c4192f58839a91446bce7aa155ae4bec
“Liana” <liana.diu@hotmail.com>
2022-03-11 10:32:29 -0800

    added photo linked to event ID

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,14 @@ function populateCardsDynamically() {
             allEvents.forEach(doc => {
                 var eventTitle = doc.data().title; //gets the name field
                 var eventDetails = doc.data().details; //gets the name field
-                var hikeID = doc.data().id; //gets the unique ID field
+                var eventID = doc.data().id; //gets the unique ID field
                 var eventTime = doc.data().time.toDate(); //gets the length field
                 let testEventCard = eventCardTemplate.content.cloneNode(true);
                 testEventCard.querySelector('.card-title').innerHTML = eventTitle;
                 testEventCard.querySelector('.card-time').innerHTML = eventTime;
                 testEventCard.querySelector('.card-details').innerHTML = eventDetails;
-                testEventCard.querySelector('a').onclick = () => setHikeData(hikeID);
-                testEventCard.querySelector('img').src = `./images/${hikeID}.jpg`;
+                testEventCard.querySelector('a').onclick = () => setEventData(eventID);
+                testEventCard.querySelector('img').src = `./images/${eventID}.jpg`;
                 eventCardGroup.appendChild(testEventCard);
             })
 
