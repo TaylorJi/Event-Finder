60e3b0b027cfca38a55a269d7e971af81c1be631
“Liana” <liana.diu@hotmail.com>
2022-03-30 13:12:00 -0700

    Edited main html and js to populate data based on new database

--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,17 @@ function populateCardsDynamically() {
         .then(allEvents => {
             allEvents.forEach(doc => {
                 var eventTitle = doc.data().title; //gets the name field
-                var eventDetails = doc.data().details; //gets the name field
-                var eventID = doc.data().id; //gets the unique ID field
-                var eventTime = doc.data().time.toDate(); //gets the length field
+                var eventDetails = doc.data().body; //gets the name field
+                var eventID = doc.data().category; //gets the unique ID field
+                var eventTimeStart = doc.data().timeStart.toDate(); //gets the length field
+                var eventTimeEnd = doc.data().timeEnd.toDate(); //gets the length field
+                var docID = doc.id;
                 let testEventCard = eventCardTemplate.content.cloneNode(true);
                 testEventCard.querySelector('.card-title').innerHTML = eventTitle;
-                testEventCard.querySelector('.card-time').innerHTML = eventTime;
+                testEventCard.querySelector('.card-time-start').innerHTML = eventTimeStart;
+                testEventCard.querySelector('.card-time-end').innerHTML = eventTimeEnd;
                 testEventCard.querySelector('.card-details').innerHTML = eventDetails;
-                testEventCard.querySelector('a').onclick = () => setEventData(eventID);
+                testEventCard.querySelector('a').onclick = () => setDocData(docID);
                 testEventCard.querySelector('img').src = `./images/${eventID}.jpg`;
                 eventCardGroup.appendChild(testEventCard);
             })
@@ -21,3 +24,7 @@ function populateCardsDynamically() {
         })
 }
 populateCardsDynamically();
+
+function setDocData(id){
+    localStorage.setItem ('documentID', id);
+}
