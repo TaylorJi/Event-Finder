2119983774eed3464ec0abaf38ab48b0dd24af9e
Taylor Sangwoo Ji <taylor.ji719@gmail.com>
2022-04-02 18:12:45 -0700

    fixed saved.js to bring saved events from database

--- scripts/saveEvent.js
+++ scripts/saveEvent.js
@@ -14,32 +14,24 @@ function getBookmarks(user) {
             console.log(bookmarks);
 
             let CardTemplate = document.getElementById("CardTemplate");
-            bookmarks.forEach(thisdocID => {
-                console.log(thisdocID);
-                db.collection("events").where("id", "==", thisdocID).get().then(snap => {
-                    size = snap.size;
-                    queryData = snap.docs;
+            bookmarks.forEach(async currentDocId => {
+                console.log(currentDocId);
+                const query = db.collection("events").doc(currentDocId);
+                const doc = await query.get();
+                if (doc.exists) {
+                    const data = doc.data();
+                    var docTitle = data.title;
+                    var docCategory = data.category;
+                    let newCard = CardTemplate.content.cloneNode(true);
 
-                    if (size == 1) {
-                        var doc = queryData[0].data();
-                        var eventTitle = doc.name; //gets the name field
-                        var eventID = doc.city; //gets the unique ID field
-                        var docID = doc.id;
-                        // var eventDetails = doc.body;
-                        // var eventCategory = doc.imgID;
-                        // var hikeLength = doc.length; //gets the length field
-                        let newCard = CardTemplate.content.cloneNode(true);
-                        newCard.querySelector('.card-title').innerHTML = eventTitle;
-                        newCard.querySelector('.card-length').innerHTML = eventID;
-                        newCard.querySelector('a').onclick = () => setDocData(docID);
-                        newCard.querySelector('img').src = `./images/${docID}.jpg`;
-                        eventCardGroup.appendChild(newCard);
-                    } else {
-                        console.log("Query has more than one data")
-                    }
-
-                })
-
+                    newCard.querySelector('.card-title').innerHTML = docTitle;
+                    newCard.querySelector('.card-length').innerHTML = docCategory;
+                    newCard.querySelector('a').onclick = () => setDocData(currentDocId);
+                    newCard.querySelector('img').src = `./images/${currentDocId}.jpg`;
+                    eventCardGroup.appendChild(newCard);
+                } else {
+                    console.log("no such documentation: " + currentDocId);
+                }
             });
         })
 }
