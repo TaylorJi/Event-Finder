b93ebea7fb8dd5bb71f0a702784e724c8c4a125b
Michaela Ashlee <mmashlee@gmail.com>
2022-04-07 14:57:46 -0700

    Added comments and fixed styling (Mobile only)

--- scripts/personalInfo.js
+++ scripts/personalInfo.js
@@ -2,22 +2,22 @@ var loggedInUser;
 
 function populateInfo() {
     firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
+        // Checks to see if the user is signed in:
         if (user) {
 
-            //go to the correct user document by referencing to the user uid
+            //go to the current user document by referencing the user uid.
             currentUser = db.collection("users").doc(user.uid)
             //get the document for current user.
             currentUser.get()
                 .then(userDoc => {
-                    //get the data fields of the user
+                    //get the data fields of the user including name, email, phone, birthday and city.
                     var userName = userDoc.data().name;
                     var userEmail = userDoc.data().email;
                     var userPhone = userDoc.data().phone;
                     var userBirthday = userDoc.data().birthday;
                     var userCity = userDoc.data().city;
 
-                    //if the data fields are not empty, then write them in to the form.
+                    //Checks to see if the data fields are empty. If they aren't, populate them with the current users information.
                     if (userName != null) {
                         document.getElementById("nameInput").value = userName;
                     }
@@ -35,7 +35,7 @@ function populateInfo() {
                     }
                 })
         } else {
-            // No user is signed in.
+
             console.log("No user is signed in");
         }
     });
@@ -46,53 +46,60 @@ populateInfo();
 
 
 function edit() {
-    //allows us to edit the form fields 
-   document.getElementById('personalInfoFields').disabled = false;
+    //Allows us to edit the form fields where the element is in personalInfo.html.
+    document.getElementById('personalInfoFields').disabled = false;
 }
 
-function save(){
-    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-    userEmail = document.getElementById('emailInput').value;     //get the value of the field with id="schoolInput"
+//Allows us to save the information inputed by the user. 
+function save() {
+    /* Retrieves the values of name, email, phone, birthday and city that the user entered in the
+    fields with the specified ID's. 
+    */
+    userName = document.getElementById('nameInput').value;       
+    userEmail = document.getElementById('emailInput').value;     
     userPhone = document.getElementById('phoneInput').value;
     userBirthday = document.getElementById('birthdayInput').value;
     userCity = document.getElementById('cityInput').value;
-   
 
-    if(!userName) {userName = "<Name>"}
+    //if user does not enter a name, replace their name with <Name>.
+    if (!userName) { userName = "<Name>" }
+
+    //updates the values of the current user into firestore databse. 
     currentUser.update({
         name: userName,
         email: userEmail,
         phone: userPhone,
         birthday: userBirthday,
         city: userCity
-      })
+    })
         .then(() => {
-          console.log("Document successfully updated!");
-          document.getElementById('personalInfoFields').disabled = true;
+            console.log("Document successfully updated!");
+            document.getElementById('personalInfoFields').disabled = true;
         })
 }
 
+//declaring a function to insert the name in the personal information page.
 function insertName2() {
     firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
+        // Checks to see if the user is signed in:
         if (user) {
-            // Do something for the current logged-in user here: 
+
             console.log(user.uid);
-            //go to the correct user document by referencing to the user uid
+            //go to the current user document by referencing the user uid.
             currentUser = db.collection("users").doc(user.uid);
-            //get the document for current user.
+            //get the document for the current user and store the name in a variable called user_Name.
             currentUser.get()
                 .then(userDoc => {
                     var user_Name = userDoc.data().name;
                     console.log(user_Name);
-                    //method #1:  insert with html only
-                    //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-                    //method #2:  insert using jquery
-                    $("#nameOfUser").text(user_Name);                         //using jquery
+
+                    //insert current users name using jquery.
+                    $("#nameOfUser").text(user_Name);
                 })
         } else {
             // No user is signed in.
         }
     });
 }
+//call the function to insert current users name.
 insertName2();
--- scripts/profile.js
+++ scripts/profile.js
@@ -1,22 +1,22 @@
+
+//declaring a function to insert the name in the profile card.
 function insertName() {
     firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
-        if (user) {                                                                 
-            // Do something for the current logged-in user here: 
-            console.log(user.uid);
-            //go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").doc(user.uid);
-            //get the document for current user.
-            currentUser.get()
-                     .then(userDoc => {
-               var user_Name = userDoc.data().name;
-               console.log(user_Name);
+        // Checks to see if the user is signed in:
+        if (user) {
 
-               //method #1:  insert with html only
-               //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-               //method #2:  insert using jquery
-               $("#name-goes-here").text(user_Name);                         //using jquery
-            })
+            console.log(user.uid);
+            //go to the current user document by referencing the user uid.
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for the current user and store the name in a variable called user_Name.
+            currentUser.get()
+                .then(userDoc => {
+                    var user_Name = userDoc.data().name;
+                    console.log(user_Name);
+
+                    //insert current users name using jquery.
+                    $("#name-goes-here").text(user_Name);
+                })
         } else {
             // No user is signed in.
         }
@@ -24,24 +24,24 @@ function insertName() {
 }
 insertName();
 
+//declaring a function to insert the name in the welcome hero. 
 function insertName2() {
     firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
-        if (user) {                                                                 
-            // Do something for the current logged-in user here: 
+        // Checks to see if the user is signed in:
+        if (user) {
+
             console.log(user.uid);
-            //go to the correct user document by referencing to the user uid
+            //go to the current user document by referencing the user uid.
             currentUser = db.collection("users").doc(user.uid);
-            //get the document for current user.
+            //get the document for the current user and store the name in a variable called user_Name.
             currentUser.get()
-                  .then(userDoc => {
-               var user_Name = userDoc.data().name;
-               console.log(user_Name);
-               //method #1:  insert with html only
-               //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-               //method #2:  insert using jquery
-               $("#newName").text(user_Name);                         
-            })
+                .then(userDoc => {
+                    var user_Name = userDoc.data().name;
+                    console.log(user_Name);
+
+                    //insert current users name using jquery.
+                    $("#newName").text(user_Name);
+                })
         } else {
             // No user is signed in.
         }
